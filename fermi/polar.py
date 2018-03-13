c0a7794b37b6482cf1c473e04c38c187b1763124
Saeran Vasanthakumar <saeranv@gmail.com>
2018-03-13 02:10:25 -0400

    polar exp

--- fermi/polar.py
+++ fermi/polar.py
@@ -2,6 +2,7 @@ import math
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
+
 def cheat():
     """ numpy cheat """
     vec = np.array([1.,2.,3.])
@@ -17,37 +18,28 @@ def cheat():
 
 def rsq_deep():
     # https://stackoverflow.com/questions/4788892/draw-square-with-polar-coordinates
-    ce = 60
+    ce = 360
     st = 5
-    rsq = [1]*((ce)/st)
+    rad = []
+    c = 0
     for phi in np.arange(0,ce,st):
-        print  "deg ", phi, (phi+45)%90-45, "cos ", math.cos(phi)
+        #print c
+        #c+=1
+        #print  "deg ", phi, (phi+45)%90-45, "cos ", math.cos(phi)
         phi_ = ((phi+45)%90-45)/180.*math.pi
-        radius = 10./math.cos(phi_)
-        rsq[int(phi/st)] = radius
+        #phi_ = phi*180.*math.pi
+        r = 5./math.cos(phi_)
+        rad.append(r)
 
     theta = np.arange(0,ce/st)
 
-    x = map(lambda t: math.cos(t*st*math.pi/180.)*rsq[t], theta)
-    y = map(lambda t: math.sin(t*st*math.pi/180.)*rsq[t], theta)
-    plt.plot(x,y,'r')
+    for inc in xrange(3):
+        inc = inc
+        x = map(lambda t: math.cos(t*st*math.pi/180.)*(rad[t] - (5.+rad[t])), theta)
+        y = map(lambda t: math.sin(t*st*math.pi/180.)*(rad[t] - (5.+rad[t])), theta)
 
+        plt.plot(x,y,'r')
 
-def main():
-    """
-    Use polar coordinates to draw a square
-    """
-    r = 10
-    theta = np.arange(0,361,1)
-    x = map(lambda t: math.cos(t*math.pi/180.)*r, theta)
-    y = map(lambda t: math.sin(t*math.pi/180.)*r, theta)
-    plt.plot(x,y,'r')
-
-    rsq = lambda t:10./math.cos(((t+45)%90-45)/180.*math.pi)
-
-    x = map(lambda t: math.cos(t*math.pi/180.)*rsq(t), theta)
-    y = map(lambda t: math.sin(t*math.pi/180.)*rsq(t), theta)
-    plt.plot(x,y,'r')
 
 def plot():
     plt.plot(np.arange(-20,21,1), [0]*41,'b') # x axis
@@ -58,7 +50,6 @@ def plot():
     plt.show()
 
 if __name__ == "__main__":
-    #main()
     rsq_deep()
 
     # ctrl w to close plot window
