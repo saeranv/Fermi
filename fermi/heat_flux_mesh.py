8dfa9ad38f4d489f3c1e16686a2453a173cba2c4
Saeran Vasanthakumar <saeranv@gmail.com>
2018-03-24 20:38:27 -0400

    starting heat flux plotter

---
+++ fermi/heat_flux_mesh.py
@@ -0,0 +1,41 @@
+import os
+import sys
+
+import math
+import numpy as np
+import pandas
+import matplotlib.pyplot as plt
+
+import openstudio_python
+
+import pprint
+
+pp = lambda p: pprint.pprint(p)
+
+def plot_flux():
+
+    osm_file_path = "C:\\ladybug\\hb_office\\OpenStudio\\hb_office.osm"
+    osm = openstudio_python.load_osm(osm_file_path)
+
+
+    surf_vector = np.array(
+        filter(lambda s: s.outsideBoundaryCondition()=="Outdoors",
+            osm.getSurfaces()))
+
+    L = map(lambda s:s.vertices(),surf_vector)
+
+    pp(L)
+    """
+    for i in xrange(len(surf_vector)):
+        pt_vector = surf_vector[i].vertices()
+        surf_vector[i]
+        for i in xrange(pt_vector.Count):
+            print pt_vector[i].x(), pt
+    """
+
+
+
+
+
+if __name__ == "__main__":
+    out = plot_flux()
--- fermi/openstudio_python.py
+++ fermi/openstudio_python.py
@@ -1,12 +1,13 @@
 import os
 import sys
-import math
 
-def main():
+
+RESOURCE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__),"..", "resources"))
+RESOURCE_OSM = os.path.join(RESOURCE_DIR, "Office.osm")
+
+def load_osm(osm_file_path=0):
 
     openstudio_dir = "C:\\Program Files\\OpenStudio 1.12.0\\CSharp\\openstudio"
-
-    resource_dir = os.path.abspath(os.path.join(os.path.dirname(__file__),"..", "resources"))
 
     if openstudio_dir not in sys.path:
         sys.path.append(openstudio_dir)
@@ -16,38 +17,19 @@ def main():
     clr.AddReference("OpenStudio")
     import OpenStudio as ops
 
-    #print dir(ops.Model)
 
-    osm_file_path = os.path.join(resource_dir, "Office.osm")
+    if not osm_file_path:
+        osm_file_path = RESOURCE_OSM
 
     if not os.path.exists(osm_file_path):
         print "error at ", osm_file_path
         return 0
-    """
-    f = open(osm_file_path)
-    for i,line in enumerate(f):
-        print line
-        if i > 10:
-            break
-    f.close()
-    """
 
-    #print ops.Model.__doc__
-    #print ops.Model.load(osm_file_path)
     osm_path = ops.Path(osm_file_path)
-    #print ops.Model.load.__doc__
 
-    office = ops.Model.load(osm_path).get()
-    surface_vector = office.getSurfaces()
+    osm = ops.Model.load(osm_path).get()
 
-    print surface_vector.Count
-
-    #for i in xrange(surface_vector.Count):
-    #    print i
-    #for d in dir(ops.Model):
-    #    print d
-
-    return 1
+    return osm
 
 if __name__ == "__main__":
-    is_run = main()
+    osm = load_osm()
