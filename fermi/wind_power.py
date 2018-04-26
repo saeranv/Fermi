374dd8de633acd349df3bd9a3506378501818d4b
Saeran Vasanthakumar <saeranv@gmail.com>
2018-04-26 19:59:57 -0400

    adding sin theta back to large wind turbine

--- fermi/wind_power.py
+++ fermi/wind_power.py
@@ -9,10 +9,10 @@ import matplotlib
 
 #Define graph constants
 plt.xlabel('Hr')
-plt.ylabel('dE/dt, E (mW), uMod')
-plt.title("Wind Energy by Hour")
+plt.ylabel('E (mWh)')
+plt.title("Wind Energy (mWh) by Hour")
 plt.grid()
-plt.axis([0,8760,0,500])
+plt.axis([0,8760,0,1000])
 
 def main():
     bash_epw.set_uwg()
@@ -24,7 +24,7 @@ def main():
 
     # windshare specs
     turbine_ht_array = [25.,45.,65.]#np.arange(25.)#15.,60.,15.)   # m
-    turbine_radius_array = [3.,12,21.]#25. # m length of blade
+    turbine_radius_array = [3.,6,21.]#25. # m length of blade
 
     #turbine_swept_area = 7.5  # m2
     #turbine_dir = 3*math.pi/2. # West in radians
@@ -45,7 +45,11 @@ def main():
     def Pcalc(turbine_diameter, v_z_):
         return (0.125 * air_density * math.pi * math.pow(turbine_diameter,2) * math.pow(v_z_,3) * perf_coef) / 1000.0 # Power (kW) at time step t
     def Ucalc(turbine_ht_, i_):
-        return u_spd[i_] * ((math.log(turbine_ht_/0.5)) / (math.log(10.0/0.5))) # from LB
+        # Apparently wind turbines can rotate to the wind....
+        v = u_spd[i_] * ((math.log(turbine_ht_/0.5)) / (math.log(10.0/0.5))) # from LB
+        if turbine_radius > 3:
+            v = abs(v * math.sin(u_dir[t + 1]))   # account for direction of wind
+        return v
 
     h = 1 # infinitisemal timestep
     #print wind_prof
@@ -77,14 +81,13 @@ def main():
                 #reference_roughness_length = 0.5?
                 V[t+1] = Ucalc(turbine_ht,t+1)
 
-                # Apparently wind turbines can rotate to the wind....
-                #v_z = abs(v_z * math.sin(u_dir[t + 1]))   # account for direction of wind
-
                 #P[t+1] = (0.5 * v_z**3 * air_density * turbine_swept_area) * perf_coef / 1000.0 # Power (kW) at time step t
                 P[t+1] = Pcalc(turbine_radius*2, V[t+1])
                 E[t+1] = E[t] + h * P[t] # kWh
-
-                #print P[t+1], E[t+1], round(v_z,2)#, epw.staUmod[t+1]
+                # Should be approx 650 kWh at 12 m/s
+                if turbine_radius > 15 and turbine_ht > 55:
+                    #print 'tblade={} & tht={}'.format(turbine_radius, turbine_ht)
+                    print P[t+1], round(V[t+1],2), abs(math.sin(u_dir[t+1]))
 
             print round(E[-1],2), "kWh,", round(E[-1]/5000.0,2)
 
