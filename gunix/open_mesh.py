55ae7af9a82f6766709740c79c0432e31890c8a7
Saeran Vasanthakumar <saeranv@gmail.com>
2018-05-22 18:27:10 -0400

    Attempting surface coloring in matplotlib

--- gunix/open_mesh.py
+++ gunix/open_mesh.py
@@ -7,6 +7,7 @@ from mpl_toolkits import mplot3d
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
+import math
 
 def test():
     # Characteristic length
@@ -20,11 +21,16 @@ def test():
 
     # Vertices of a square hole
     squareHoleCoordinates = np.array([
-        [1, 1, 0],
-        [4, 1, 0],
-        [4, 4, 0],
-        [1, 4, 0]
+        [0,0,0],
+        [5,0,0],
+        [5,5,0],
+        [0,5,0],
         ])
+        #[1, 1, 0],
+        #[4, 1, 0],
+        #[4, 4, 0],
+        #[1, 4, 0]
+        #])
 
     # Create geometric object
     geom = pygmsh.built_in.Geometry()
@@ -32,7 +38,7 @@ def test():
     # Create square hole
     squareHole = geom.add_polygon(
         squareHoleCoordinates, lcar,
-        make_surface=True
+        make_surface=False
         )
 
     # Create square domain with square hole
@@ -43,8 +49,8 @@ def test():
 
     points, cells, point_data, cell_data, field_data = pygmsh.generate_mesh(geom)
 
-    print 'points'
-    print points
+    #print 'points'
+    #print points
     """
     print 'cells'
     print cells
@@ -63,7 +69,7 @@ def test():
 def view_mesh(ptvec):
 
     ig = plt.figure()
-    #ax = plt.axes(projection="3d")
+    ax = plt.axes(projection="3d")
 
     # Get x,y,z coordinates as np arrays
     x = np.array([ptvec[i][0] for i in xrange(len(ptvec))])
@@ -73,10 +79,23 @@ def view_mesh(ptvec):
     xx,yy = np.meshgrid(x,y)
     #z = f(xx,yy)
     #z = np.array([ptvec[i][2] for i in xrange(len(ptvec))])
+    zz = np.zeros((len(ptvec), len(ptvec)))
 
-    plt.plot(xx, yy, marker='.', color='k', linestyle='none')
-
-    #ax.plot_surface(x,y,z, rstride=1, cstride=1, cmap="viridis", edgecolor="none")
+    # This can be done in parallel
+    ln = float(len(ptvec))
+    r = 1.0
+    f = lambda x: (x/(ln-1))*(360/r)*(math.pi/180.0)
+    for i in xrange(len(ptvec)):
+        theta = f(i)
+        print i, theta
+        zx = math.cos(theta)
+        for j in xrange(len(ptvec)):
+            theta = f(j)
+            zy = 0#math.sin(theta)
+            zz[i,j] = (zx)#*zy)
+    #plt.plot(xx, yy, zz, marker='.', color='k', linestyle='none')
+    ax.scatter3D(xx, yy, zz) #c=zpt, cmap="Reds")
+    #ax.plot_surface(xx,yy, rstride=1, cstride=1, cmap="viridis", edgecolor="none")
 
     plt.show()
 
