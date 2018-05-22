b621426e66bd01ad75ed3dc88f6ab36b20041ada
Saeran Vasanthakumar <saeranv@gmail.com>
2018-05-22 00:36:13 -0400

    open_mesh t oview_mesh

--- gunix/open_mesh.py
+++ gunix/open_mesh.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-import numpy as np
-import pygmsh
 
+import pygmsh
+from mpl_toolkits import mplot3d
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
 
 def test():
     # Characteristic length
@@ -29,37 +32,57 @@ def test():
     # Create square hole
     squareHole = geom.add_polygon(
         squareHoleCoordinates, lcar,
-        make_surface=False
+        make_surface=True
         )
 
     # Create square domain with square hole
-    geom.add_rectangle(
-        xmin, xmax, ymin, ymax, 0.0, lcar,
-        holes=[squareHole.line_loop]
-        )
+    #geom.add_rectangle(
+    #    xmin, xmax, ymin, ymax, 0.0, lcar,
+    #    holes=[squareHole.line_loop]
+    #    )
 
     points, cells, point_data, cell_data, field_data = pygmsh.generate_mesh(geom)
-    """
+
     print 'points'
     print points
+    """
     print 'cells'
     print cells
     print 'point data'
     print point_data
     print 'field_data'
     print field_data
+    """
     # TODO support for volumes of triangle6
     # ref = 16.0
     # from helpers import compute_volume
     # assert abs(compute_volume(points, cells) - ref) < 1.0e-2 * ref
-    """
+
     return points, cells
 
-def view_mesh():
-    pass
+def view_mesh(ptvec):
+
+    ig = plt.figure()
+    #ax = plt.axes(projection="3d")
+
+    # Get x,y,z coordinates as np arrays
+    x = np.array([ptvec[i][0] for i in xrange(len(ptvec))])
+    y = np.array([ptvec[i][1] for i in xrange(len(ptvec))])
+
+    ##f = lambda x,y: np.sqrt(x**2 + y**2)
+    xx,yy = np.meshgrid(x,y)
+    #z = f(xx,yy)
+    #z = np.array([ptvec[i][2] for i in xrange(len(ptvec))])
+
+    plt.plot(xx, yy, marker='.', color='k', linestyle='none')
+
+    #ax.plot_surface(x,y,z, rstride=1, cstride=1, cmap="viridis", edgecolor="none")
+
+    plt.show()
 
 
 if __name__ == '__main__':
     import meshio
     meshio.write('hole_in_square.vtu', *test())
-    view_mesh()
+    pts, clls = test()
+    view_mesh(pts)
--- gunix/view_mesh.py
+++ gunix/view_mesh.py
@@ -23,19 +23,23 @@ def main():
     ypt = np.cos(zpt) + 0.1 * np.random.randn(100)
     ax.scatter3D(xpt, ypt, zpt) #c=zpt, cmap="Reds")
     """
-    
+
     f = lambda x,y: np.sin(np.sqrt(x**2 + y**2))
     x = np.linspace(-10,10,50)
     y = np.linspace(-10,10,50)
     xx,yy = np.meshgrid(x,y)
     z = f(xx,yy)
 
+    #pp(x)
+    #pp(xx)
+    pp(z)
+
     ax.plot_surface(xx,yy,z, rstride=1, cstride=1, cmap="viridis", edgecolor="none")
     ax.set_title("surface")
+
     # Rotate view by:
     # - elevation: 60 degrees aboce x,y plane
     # - azimuth: 35 counter clockwise from z-axis
-
     ax.view_init(60,35)
 
 def plot():
