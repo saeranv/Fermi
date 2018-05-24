686e0935d8ef7089892b097187b27bf1ae31f255
Saeran Vasanthakumar <saeranv@gmail.com>
2018-05-23 21:07:45 -0400

    Adding progress bar changes

--- fermi/progressbar.py
+++ fermi/progressbar.py
@@ -1,44 +1,48 @@
-# Print iterations progress
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+import sys
 
-def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█'):
+def print_progress(iteration, total, prefix='', suffix='', bar_length=50):
     """
-    From https://stackoverflow.com/questions/3173320/text-progress-bar-in-the-console
+    This is a modified version from:
+    https://gist.github.com/aubricus/f91fb55dc6ba5557fbab06119420dd6a
+    https://stackoverflow.com/questions/3173320/text-progress-bar-in-the-console/34325723#34325723
 
     Call in a loop to create terminal progress bar
-    @params:
-        iteration   - Required  : current iteration (Int)
-        total       - Required  : total iterations (Int)
-        prefix      - Optional  : prefix string (Str)
-        suffix      - Optional  : suffix string (Str)
-        decimals    - Optional  : positive number of decimals in percent complete (Int)
-        length      - Optional  : character length of bar (Int)
-        fill        - Optional  : bar fill character (Str)
+        iteration   - Required  : current iteration (int)
+        total       - Required  : total iterations (int)
+        prefix      - Optional  : prefix string (str)
+        suffix      - Optional  : suffix string (str)
+        bar_length  - Optional  : character length of bar (int)
     """
-    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
-    filledLength = int(length * iteration // total)
-    bar = fill * filledLength + '-' * (length - filledLength)
-    print '\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix), end = '\r'
-    # Print New Line on Complete
+
+    str_format = "{0:." + str(1) + "f}"
+    percents = str_format.format(100 * (iteration / float(total)))
+    filled_length = int(round(bar_length * iteration / float(total)))
+    bar = '|' * filled_length + '-' * (bar_length - filled_length)
+
+    sys.stdout.write("\r{} |{}| {}{} {}".format(prefix, bar, percents, '%', suffix)),
+
     if iteration == total:
-        print
+        sys.stdout.write('\n')
 
-#
-# Sample Usage
-#
+    sys.stdout.flush()
 
-from time import sleep
 
-# A List of Items
-items = list(range(0, 57))
-l = len(items)
+if __name__ == "__main__":
+    from time import sleep
 
-# Initial call to print 0% progress
-printProgressBar(0, l, prefix = 'Progress:', suffix = 'Complete', length = 50)
-for i, item in enumerate(items):
-    # Do stuff...
-    sleep(0.1)
-    # Update Progress Bar
-    printProgressBar(i + 1, l, prefix = 'Progress:', suffix = 'Complete', length = 50)
+    # A List of Items
+    items = list(range(0, 57))
+    l = len(items)
 
-# Sample Output
-#Progress: |█████████████████████████████████████████████-----| 90.0% Complete
+    # Initial call to print 0% progress
+    printProgressBar(0, l, prefix = 'Progress:', suffix = 'Complete', bar_length = 50)
+    for i, item in enumerate(items):
+        # Do stuff...
+        sleep(0.1)
+        # Update Progress Bar
+        printProgressBar(i + 1, l, prefix = 'Progress:', suffix = 'Complete', bar_length = 50)
+
+    # Sample Output
+    #Progress: |█████████████████████████████████████████████-----| 90.0% Complete
