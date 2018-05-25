76b4bb84147c1254c72875e6ef5eb6b6787b3cc2
Saeran Vasanthakumar <saeranv@gmail.com>
2018-05-25 19:08:17 -0400

    Finally got a decent functional programming-style HB script

--- gunix/functional_honeybee.py
+++ gunix/functional_honeybee.py
@@ -12,10 +12,16 @@ except: pass
 import scriptcontext as sc
 
 import rhinoscriptsyntax as rs
+import Rhino as rc
 import scriptcontext as sc
+import math
+
 
 from pprint import pprint
 pp = pprint
+
+# Debug
+dbg = []
 
 
 """
@@ -24,13 +30,6 @@ Functional programming with Honeybee.
 For more intuitive data management.
 
 """
-
-
-
-# Debug
-dbg = []
-
-get_program = lambda z: z.name.split("_")[0]
 
 def list_to_tree(input, none_and_holes=True, source=[0]):
     #https://gist.github.com/piac/ef91ac83cb5ee92a1294
@@ -128,18 +127,69 @@ def get_srf_lst(zone):
            ceilings, floors, exposedFloors, groundFloors, undergroundWalls, \
            undergroundSlabs, undergroundCeilings, shadings]
 
+def is_near_zero(n,eps=1e-14):
+    return abs(n) < eps
+
+import functools
+
+def memoize(obj):
+    #https://wiki.python.org/moin/PythonDecoratorLibrary#Memoize
+    cache = obj.cache = {}
+
+    @functools.wraps(obj)
+    def memoizer(*args, **kwargs):
+        key = str(args) + str(kwargs)
+        if key not in cache:
+            cache[key] = obj(*args, **kwargs)
+        return cache[key]
+    return memoizer
+
+def apply2depth(L,d=-1,f=lambda x:x):
+    @memoize
+    def get_depth(L_,d_):
+        # Assume balanced tree
+        if type(L_)!=type([]):
+            return d_
+        else:
+            return get_depth(L_[0],d_+1)
+
+    def apply2depth_(L_, d_, dinc_, f_):
+        # Applies function f to node d
+        if is_near_zero(d_ - dinc_):
+            return f(L_)
+        else:
+            return [apply2depth_(L_[i],d_,dinc_+1,f_) for i in xrange(len(L_))]
+
+    #dmax: max depth
+    #d: target depth
+    #dinc: current depth
+
+    dmax = get_depth(L,0) # to make sure 0 is == first index start at -1
+    d = d if d > -0.5 else dmax + d + 1  # Modify d if negative index is used as target depth
+
+    return apply2depth_(L,d,0,f)
+
+def get_wall_by_direction(srf,refv,tol=45):
+    srf.normalVector.Unitize()
+    orient_vector = rc.Geometry.Vector3d(refv[0],refv[1],refv[2])
+    # Calculate the tolerance as the dot product
+    tol_as_dot = 1.0 - math.cos(tol*(math.pi/180.0))
+    dot_prod = srf.normalVector * orient_vector
+
+    if is_near_zero(dot_prod-1.0,tol_as_dot):
+        return srf.geometry
+    else:
+        return None
 
 # Call the objects from the lib
 hb_hive = sc.sticky["honeybee_Hive"]()
 zonelst = hb_hive.visualizeFromHoneybeeHive(hbzone)
 
-
-
-# See object properties
-#dbg.extend(dir(zonelst[0]))
+#-----------------------------------------------------#
+# Add hb room from honeybee_room.py
 
 # Use dictionary to sort into nested list by program
-nested_zone = nest_lst_by_prop(zonelst,get_program)
+nested_zone = nest_lst_by_prop(zonelst, lambda z: z.name.split("_")[0])
 
 # List index for surface types
 # 0  walls
@@ -158,55 +208,45 @@ nested_zone = nest_lst_by_prop(zonelst,get_program)
 # 13 undergroundCeilings
 # 14 shadings
 
-nested_srf = [
-        [get_srf_lst(nested_zone[i][j]) for j in xrange(len(nested_zone[i]))]
-            for i in xrange(len(nested_zone))]
+# nest_srf: tree depth structure
+# 0 program type
+# 1 zone type
+# 2 surfaces list [[wallsrfs][roofsrfs]...[ceilingsrfs]]
+# 3 list of surface types
+# 4 surfaces
 
-# Get the nested surfaces
-# use srf.geometry or srf.punchedGeometry
+# N.B to get the nested surfaces use srf.geometry
+# or srf.punchedGeometry
 
-get_geo = lambda s:[s[i].geometry for i in xrange(len(s))]
+# Go to root (zones) and replace zone objects with list of surfaces
+nest_srf = apply2depth(nested_zone,d=-1,f=get_srf_lst)
 
 # Get ceilings
-nest_ceiling = [
-        [get_geo(nested_srf[i][j][7]) for j in xrange(len(nested_srf[i]))]
-            for i in xrange(len(nested_srf))]
+nest_ceiling = apply2depth(nest_srf,2,lambda srflst: [s.geometry for s in srflst[7]])
 
 # Get floors
-nest_floor = [[
-                [s.geometry                                     # operation
-                    for s in nested_srf[i][j][8]]               # surfaces
-                        for j in xrange(len(nested_srf[i]))]    # zones
-                            for i in xrange(len(nested_srf))]   # program types
+nest_floor = apply2depth(nest_srf,2,lambda srflst: [s.geometry for s in srflst[8]])
 
-#get_north = lambda s: s.
-# Get floors
-#nest_wall_N = [
-#        [get_north(nested_srf[i][j][0].geometry) for j in xrange(len(nested_srf[i]))]
-#            for i in xrange(len(nested_srf))]
-dbg.extend(dir(nested_srf[0][0][0][0]))
+# Get walls
+nest_wall = apply2depth(nest_srf,2,lambda srflst: [s for s in srflst[0]])
+
+# Get wall by orientation
+nest_wall_N = apply2depth(nest_wall,d=-2,f=lambda srflst:
+    filter(lambda n: n!=None, [get_wall_by_direction(s,(0,1,0)) for s in srflst]))
+nest_wall_E = apply2depth(nest_wall,d=-2,f=lambda srflst:
+    filter(lambda n: n!=None, [get_wall_by_direction(s,(1,0,0)) for s in srflst]))
+nest_wall_S = apply2depth(nest_wall,d=-2,f=lambda srflst:
+    filter(lambda n: n!=None, [get_wall_by_direction(s,(0,-1,0)) for s in srflst]))
+nest_wall_W = apply2depth(nest_wall,d=-2,f=lambda srflst:
+    filter(lambda n: n!=None, [get_wall_by_direction(s,(-1,0,0)) for s in srflst]))
+
+# Map back to GH trees
 ceiling = list_to_tree(nest_ceiling)
 floor = list_to_tree(nest_floor)
+exterior_wall_N = list_to_tree(nest_wall_N)
+exterior_wall_E = list_to_tree(nest_wall_E)
+exterior_wall_S = list_to_tree(nest_wall_S)
+exterior_wall_W = list_to_tree(nest_wall_W)
 
-#exterior_wall_N = list_to_tree(nest_wall_N)
 
-"""
-forfirst = False
-NL = nested_srf
-for i in xrange(len(NL)):
-    #for each type
-    type = NL[i]
-    print nested_zone[i][0].name
-    for j in xrange(len(type)):
-        # for each list of surface_lists in type
-        srflst = type[j]
-        if True:#if forfirst==False:
-            anlysrf = srflst[6]
-            dbg.extend(anlysrf)
-            print len(anlysrf)
-            #forfirst = True
-    print '--'
-"""
-
-#print 'dbg'
-#print dbg
+# now do the data management
