63514b64603c583d69d4966315f133cb5946e215
saeranv <saeranv@gmail.com>
2018-06-17 03:14:30 -0400

    adding gunix

--- src/grsshppr_gunix.py
+++ src/grsshppr_gunix.py
@@ -8,9 +8,14 @@ import shutil
 
 # https://news.ycombinator.com/item?id=16318677
 
+CURR_DIR = os.path.dirname(os.path.abspath(fpath))
+
 #script = "C:\\Users\\631\\Documents\\GitHub\\Fermi\\src\\helloworld.py"
-script = "C:\\Users\\631\\Documents\\GitHub\\Fermi\\src\\pyqt_jupyter.py"
-cmd = "C:\\Users\\631\\AppData\\Local\\Continuum\\Anaconda2\\python.exe"
+script = os.path.join(CURR_DIR,"pyqt_jupyter.py")
+if "631" in CURR_DIR:
+    cmd = "C:\\Users\\631\\AppData\\Local\\Continuum\\Anaconda2\\python.exe"
+else:
+    cmd = "C:\\Users\\user\\Anaconda2\\python.exe"
 
 def coerce_brep(guidlst):
     return [rs.coercebrep(guid) for guid in guidlst]
@@ -24,11 +29,11 @@ def dynamic_output():
         (0, "Test some stuff."),
         (1, "This could be a thing."),
         (2, "More."),
-        (3, "Even More."),
+        (3, "Yo dude."),
     ]
 
     for i in xrange(len(ghcomp_output)):
-        output_num = ghcomp_output[i][0] 
+        output_num = ghcomp_output[i][0]
         output_name = ghcomp_output[i][1]
         try:
             ghenv.Component.Params.Output[output_num].NickName = output_name
@@ -44,12 +49,12 @@ if launch:
     #hbz = call_from_hive(
     #        coerce_brep(hbz)
     #        )
+    
+    p = subprocess.Popen([cmd, script, argstr], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    print p.communicate()
 
-    #p = subprocess.Popen([cmd, script], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    #print p.communicate()
-
-    file_src = "C:\\Users\\631\\Documents\\GitHub\\Fermi\\src\\grsshppr_gunix.py"
-    file_dst = "C:\\Users\\631\\Documents\\GitHub\\Fermi\\src\\grsshppr_gunix_tmp.py"
+    file_src = os.path.join(CURR_DIR,"grsshppr_gunix.py")
+    file_dst = os.path.join(CURR_DIR,"grsshppr_gunix_tmp.py")
     if not os.path.exists(file_dst):
         shutil.copyfile(file_src, file_dst)
 
--- src/pyqt_jupyter.py
+++ src/pyqt_jupyter.py
@@ -2,22 +2,34 @@ from qtconsole.qt import QtGui
 from qtconsole.rich_jupyter_widget import RichJupyterWidget
 from qtconsole.inprocess import QtInProcessKernelManager
 
+import sys
+
+class Test(object):
+    def __init__(self,input):
+        self.input = input
+    def __repr__(self):
+        return "I am a test haha " + str(self.input)
 
 class ConsoleWidget(RichJupyterWidget):
-
-
     def __init__(self, customBanner=None, *args, **kwargs):
         super(ConsoleWidget, self).__init__(*args, **kwargs)
 
-        if customBanner is not None:
-            self.banner = customBanner
-
+        #if customBanner is not None:
+        customBanner = "Hi there saeran " + str(sys.argv[1]) + "\n\n"
+        self.banner = customBanner
         self.font_size = 6
         self.kernel_manager = kernel_manager = QtInProcessKernelManager()
         kernel_manager.start_kernel(show_banner=False)
         kernel_manager.kernel.gui = 'qt'
         self.kernel_client = kernel_client = self._kernel_manager.client()
         kernel_client.start_channels()
+
+        # test this
+        t = Test(14)
+        t1 = Test(26)
+        D = {"t":t,"ti":t1,"sysarg":sys.argv}
+        kernel = kernel_manager.kernel
+        kernel.shell.push(D)
 
         def stop():
             kernel_client.stop_channels()
