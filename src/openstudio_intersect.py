fc651913164ec53fad55e7617ea395298996ae04
Saeran Vasanthakumar <saeranv@gmail.com>
2018-07-11 02:25:47 -0400

    for discourse downloading

--- src/openstudio_intersect.py
+++ src/openstudio_intersect.py
@@ -2,6 +2,7 @@ import Rhino as rc
 import rhinoscriptsyntax as rs
 import pprint
 import scriptcontext as sc
+import ghpythonlib as ghlib
 
 pp = pprint.pprint
 
@@ -10,22 +11,6 @@ https://github.com/NREL/OpenStudio/search?q=intersect&unscoped_q=intersect
 https://github.com/NREL/OpenStudio/blob/7865ba413ef52e8c41b8b95d6643d68eb949f1c4/openstudiocore/sketchup_plugin/openstudio/sketchup_plugin/user_scripts/Alter%20or%20Add%20Model%20Elements/Intersect_Space_Geometry.rb
 https://github.com/NREL/OpenStudio/blob/7865ba413ef52e8c41b8b95d6643d68eb949f1c4/openstudiocore/sketchup_plugin/openstudio/sketchup_plugin/sketchup/Geom.rb
 """
-
-def tree_to_list(input, retrieve_base = lambda x: x[0]):
-    """Returns a list representation of a Grasshopper DataTree"""
-    def extend_at(path, index, simple_input, rest_list):
-        target = path[index]
-        if len(rest_list) <= target: rest_list.extend([None]*(target-len(rest_list)+1))
-        if index == path.Length - 1:
-            rest_list[target] = list(simple_input)
-        else:
-            if rest_list[target] is None: rest_list[target] = []
-            extend_at(path, index+1, simple_input, rest_list[target])
-    all = []
-    for i in range(input.BranchCount):
-        path = input.Path(i)
-        extend_at(path, 0, input.Branch(path), all)
-    return retrieve_base(all)
 
 if sc.sticky.has_key('honeybee_release'):
     if sc.sticky["honeybee_folders"]["OSLibPath"] != None:
@@ -65,29 +50,75 @@ else:
 def is_near_zero(val, eps=1e-6):
     return abs(val) < eps
 
-if openStudioIsReady:
+
+def get_vectors(face, points, brep_vec):
+    face_cpt = rc.Geometry.Vector3d(rs.CurveAreaCentroid(face)[0])
+
+    vec_A = rc.Geometry.Vector3d(points[1]-points[0])
+    vec_B = rc.Geometry.Vector3d(points[2]-points[1])
+
+    normal = rc.Geometry.Vector3d.CrossProduct(vec_A, vec_B)
+    normal = (face_cpt + normal) - face_cpt
+    normal_reverse = (face_cpt + normal) - face_cpt
+    normal_reverse.Reverse()
+
+    normal_reverse.Unitize()
+    normal.Unitize()
+
+    return normal, normal_reverse, face_cpt
+
+def get_norm_dist(face,points, brep_vec):
+
+    normal, normal_reverse, face_cpt = get_vectors(face, points, brep_vec)
+
+    norm_dist = (brep_vec - (face_cpt + normal)).Length
+    norm_reverse_dist = (brep_vec - (face_cpt + normal_reverse)).Length
+
+    return norm_dist, norm_reverse_dist
+
+
+def get_norm_vec(face,points, brep_vec):
+
+    normal, normal_reverse, face_cpt = get_vectors(face, points, brep_vec)
+
+    return face_cpt, normal, normal_reverse
+
+TOL = sc.doc.ModelAbsoluteTolerance
+
+if not openStudioIsReady:
+    print "Problem loading OSM. Have you let HB fly?"
+
+else:
     osm = ops.Model()
 
-    breplst = tree_to_list(srftree)
-
-
-
     # BREP
-    for i in range(len(breplst)):
-        brep = breplst[i]
-        #print 'brep'
+    for i in range(len(breps)):
+        brep = breps[i]
 
         osm_space = ops.Space(osm)
         osm_space.setName("space_{}".format(i))
 
-        #surf_vector = ops.SurfaceVector()
+        # ghlib methods
+        faces = ghlib.components.DeconstructBrep(rs.coercebrep(brep))[0]
+        brep_vec = rs.coerce3dvector(ghlib.components.Area(brep)[1])
 
         # SURFACES
-        for j in range(len(brep)):
-            face_loop = brep[j]
-            points = rs.PolylineVertices(face_loop)
-            points.reverse()
-            #points = points[:len(points)-2]
+        for j in range(len(faces)):
+
+            face = faces[j]
+
+            edges = ghlib.components.DeconstructBrep(face)[1]
+
+            face_curve = rc.Geometry.Curve.JoinCurves(edges, TOL)[0]
+            nc = face_curve.ToNurbsCurve()
+            points = [nc.Points[i_].Location for i_ in xrange(nc.Points.Count)]
+            points.pop()
+
+            # Sort the face normals of your space
+            ndist, rndist = get_norm_dist(face_curve, points, brep_vec)
+
+            if (ndist < rndist):
+                points.reverse()
 
             srf_point_vector = ops.Point3dVector()
 
@@ -95,21 +126,16 @@ if openStudioIsReady:
             for k in range(len(points)):
 
                 pt = points[k]
-
                 ops_pt = ops.Point3d(pt.X, pt.Y, pt.Z)
-
                 srf_point_vector.Add(ops_pt)
 
             # Make SURFACE
             osm_srf = ops.Surface(srf_point_vector, osm)
             osm_srf.setName("surface_{}_{}".format(i,j))
             osm_srf.setSpace(osm_space)
-            #surf_vector.Add(osm_srf)
-
 
 
     starting_surfaces = osm.getSurfaces()
-    print "Starting # surfaces: ", starting_surfaces.Count
 
     space_lst = osm.getSpaces()
 
@@ -121,6 +147,7 @@ if openStudioIsReady:
     # Intersect Masses
     for i in range(space_lst.Count):
         curr_space = space_lst[i]
+
         for j in range(space_lst.Count):
             chk_space = space_lst[j]
 
@@ -128,36 +155,33 @@ if openStudioIsReady:
             if is_near_zero(i-j):
                 continue
 
-            if True==True:#bounding_boxes[i].intersects(bounding_boxes[j]):
-                #print 'int exists'
-                #try:
-                #print [s for s in curr_space.surfaces]
+            if bounding_boxes[i].intersects(bounding_boxes[j]):
                 curr_space.intersectSurfaces(chk_space)
-                # curr_space.nameString(), chk_space.nameString(),
-                print curr_space.surfaces.Count, chk_space.surfaces.Count
-                #print '---'
                 #Util.show($"Intersected and matched {sp.nameString()} with {bSpace.nameString()}");
-                #except:
-                #  print 'fail to intersect'
-    #print dir(ops.Model)
+
     finishing_surfaces = osm.getSurfaces()
 
-    print "Finishing # surfaces: ", finishing_surfaces.Count
-
     space_lst = osm.getSpaces()
+
     # Convert back to Rhino
     intersected = []
     for i in range(space_lst.Count):
         surfaces = space_lst[i].surfaces
+        brep_faces = []
         for j in range(surfaces.Count):
             osm_points = surfaces[j].vertices()
-
             rc_points = [rc.Geometry.Point3d(p.x(), p.y(), p.z()) for p in osm_points]
-
+            rc_points += [rc_points[0]]
             crv = rc.Geometry.Curve.CreateControlPointCurve(rc_points,1)
 
-            intersected.append(crv)
+            face = ghlib.components.BoundarySurfaces(crv)
+            brep_faces.append(face)
 
-            #rc.Geometry.Surface.Create
+        joined_brep = ghlib.components.BrepJoin(brep_faces)[0]
+        intersected.append(joined_brep)
+    #osm.save(ops.Path("intersect.osm"), True)
 
-    osm.save(ops.Path("intersect.osm"), True)
+    print "{} additional surfaces were added to your {} breps.".format(
+        finishing_surfaces.Count - starting_surfaces.Count,
+        osm.getSpaces().Count
+        )
