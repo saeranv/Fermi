e8fa25b9af18aaade867e7227abcb7981b50c990
Saeran Vasanthakumar <saeranv@gmail.com>
2018-11-02 16:47:09 -0400

    experimenting with mapping polar coordinates to orthogonal shapes

--- src/polar.py
+++ src/polar.py
@@ -3,6 +3,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sys
 
+"""
+function getLengthForDeg(phi){
+    phi = ((phi+45)%90-45)/180*Math.PI;
+    return 1/Math.cos(phi);
+"""
 def cheat():
     """ numpy cheat """
     vec = np.array([1.,2.,3.])
@@ -18,14 +23,14 @@ def cheat():
 
 def circle_to_square():
     """ https://stackoverflow.com/questions/4788892/draw-square-with-polar-coordinates """
-    st = 5
+    st = 60
     ce = 360 + st
     rad = []
     c = 0
     sqlen = 10
     for phi in np.arange(0,ce,st):
         #print  "deg ", phi, (phi+45)%90-45, "cos ", math.cos(phi)
-        phi_ = ((phi+45)%90-45)/180.*math.pi
+        phi_ = ((phi*45.0) % 90.0-45.0) * (math.pi/180.0)
         #phi_ = phi*180.*math.pi
         r = sqlen/math.cos(phi_)
         rad.append(r)
@@ -52,8 +57,8 @@ def circle_to_square_iterative():
     sqlen = 10
     for phi in np.arange(0,ce,st):
         #print  "deg ", phi, (phi+45)%90-45, "cos ", math.cos(phi)
-        phi_ = ((phi+45)%90-45)/180.*math.pi
-        #phi_ = phi*180.*math.pi
+        phi_ = ((phi*45) % 90-45) * (math.pi/180.0)
+        # phi_ = phi*180.*math.pi
         r = sqlen/math.cos(phi_)
         rad.append(r)
 
@@ -80,9 +85,29 @@ def plot():
     plt.axis([-20,20,-20,20])
     plt.show()
 
+def circle_to_square_dot():
+    theta = range(90)
+    radius = 10.0
+    xlst = []
+    ylst = []
+    ref_vec = np.array([0,1])
+
+    for deg in theta:
+        x = math.cos(deg) * radius
+        y = math.sin(deg) * radius
+        vec = np.array([x,y])
+        unit_vec = vec/np.linalg.norm(vec)
+        proj_vec = np.dot(unit_vec, ref_vec) * ref_vec * radius
+        proj_vec = proj_vec + np.array([radius,0])
+        xlst.append(proj_vec[0])
+        ylst.append(proj_vec[1])
+    print(ref_vec)
+    plt.plot(xlst, ylst,'r')
+
 if __name__ == "__main__":
     #circle_to_square_iterative()
-    circle_to_square()
+    #circle_to_square()
+    circle_to_square_dot()
 
     # ctrl w to close plot window
     if len(sys.argv)> 1 and sys.argv[1]=="p":
